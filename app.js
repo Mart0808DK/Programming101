9b754cd7bcfaaddef2c57fc47483ee8afa6f1229
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-01-31 12:24:58 +0100

    Ændringer i Htlm filen

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ document.body.innerHTML += "Hello JavaScript!";
 console.log("Hello Dat.js");
 console.log("Hello Students");
 console.log("Hello World");
+
+function sayHello() {
+  document.querySelector("h3").textContent =
+    "Hej " + document.querySelector("Martin").value;
+
+  document.querySelector("button").addEventListener("click", sayHello);
+}
