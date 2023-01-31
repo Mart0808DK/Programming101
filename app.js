4c42356c7d5ffcfb54a14b5c21abdb2622486fc3
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-01-31 12:54:34 +0100

    Nye ændringer
    Har lavet nogle andre ændringer

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-document.body.innerHTML += "Hello JavaScript!";
-// alert("Hello dat.js");
-console.log("Hello Dat.js");
-console.log("Hello Students");
-console.log("Hello World");
-
 function sayHello() {
   document.querySelector("h3").textContent =
-    "Hej " + document.querySelector("Martin").value;
-
-  document.querySelector("button").addEventListener("click", sayHello);
+    "Hej " + document.querySelector("input").value;
 }
+
+document.querySelector("button").addEventListener("click", sayHello);
