52aa238daadd1763e942e3437c6ad2171fefc333
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-01-31 13:19:56 +0100

    Nye æn

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 function sayHello() {
   document.querySelector("h3").textContent =
-    "Hej " + document.querySelector("input").value;
+    "Hej " +
+    document.querySelector("#name").value +
+    " din alder er " +
+    document.querySelector("#age").value +
+    " " +
+    document.querySelector("#email").value;
 }
 
 document.querySelector("button").addEventListener("click", sayHello);
